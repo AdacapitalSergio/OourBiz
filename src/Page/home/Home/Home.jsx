110e2129fdcbe8113cd088259641a2df310e7224
Admilson Afonso <admilsoafonson@exemplo.com>
2025-05-11 13:36:01 +0100

    ss

--- src/Page/home/Home/Home.jsx
+++ src/Page/home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Head } from '../../../componentes/head/Head'
+import { Inicio } from '../Inicio/inicio'
 
 export const Home = () => {
   return (
     <div>
       <Head/>
+      <Inicio/>
     </div>
   )
 }
---
+++ src/Page/home/Inicio/Inicio.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './inicio.css'
+
+export const Inicio = () => {
+  return (
+    <div className='fundo'>
+        <div className="segundo">
+            
+
+            <h1 className='segundo-h1'>Transforme o seu negócio</h1>
+            <p className='segundo-p'>O mercado está em constante movimento. Mas e você? Está acompanhando as transformações? A Ourbiz oferece soluções inteligentes e inovadoras em consultoria de gestão empresarial
+                 para desenvolver o potencial do seu negócio.</p>
+
+            <a className='btn-consulta' href="">Consulte os nossos serviços</a>
+
+        
+
+        </div>
+
+    </div>
+  )
+}
