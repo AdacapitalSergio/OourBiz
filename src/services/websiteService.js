30bd5df7b92a27b72f9bd45a516c3312cb58f00b
NeizyPaulo-100 <noetchipukupaulo@gmail.com>
2025-08-11 18:23:46 +0100

    Pequenas alteracoe nos services

--- src/services/websiteService.js
+++ src/services/websiteService.js
@@ -1,30 +1,27 @@
 import axios from "axios";
 
-export async function enviarFormularioWebsite(dados) {
-  try {
-    const response = await axios.post(
-      "https://api.v1.ourbiz.ao/api/solitar",
-      dados,
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
+const API_URL2 = "https://api.v1.ourbiz.ao/api"
 
-    return response.data; // Axios já retorna o JSON no .data
-  } catch (error) {
-    console.error("Erro ao enviar formulário:", error);
-    throw new Error("Erro ao enviar formulário");
-  }
-}
+export const enviarFormularioWebsite = async (dados) => {
+    try {
+        const response = await axios.post(`${API_URL2}/solicitar/`, dados);
+        console.log(dados)
+        return response.data;
+    } catch (error) {
+        console.log(dados)
+        console.error("Erro ao enviar o formulário:", error);
+        throw error;
+    }
+};
 
 export const sendContactMessage = async (contactData) => {
-  try {
-    const response = await axios.post('https://api.v1.ourbiz.ao/api/solitar/cotacto', contactData);
-    return response.data;
-  } catch (error) {
-    console.error('Erro ao enviar mensagem de contato:', error);
-    throw error;
-  }
+    try {
+        const response = await axios.post(`${API_URL2}/solicitar/cotacto/`, contactData);
+        console.log(contactData)
+        return response.data;
+    } catch (error) {
+        console.log(contactData)
+        console.error("Erro ao enviar o formulário:", error);
+        throw error;
+    }
 };
